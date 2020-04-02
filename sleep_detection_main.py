5c1e5cae4f195b632d47cd238e89db21ab9103d1
Minyu Gu <guminyu94@gmail.com>
2020-04-02 16:41:44 -0500

    p8_5

--- sleep_detection_main.py
+++ sleep_detection_main.py
@@ -9,9 +9,9 @@ from matplotlib import pyplot as plt
 import mxnet as mx
 from mxnet.gluon import nn
 from matplotlib import patches
-#import skvideo
-#skvideo.setFFmpegPath('C:/Users/gumin/Desktop/machineLearning/ffmpeg-20200315-c467328-win64-shared/bin')
-#import skvideo.io
+import skvideo
+skvideo.setFFmpegPath('C:/Users/gumin/Desktop/machineLearning/ffmpeg-20200315-c467328-win64-shared/bin')
+import skvideo.io
 
 def video_cap(filename):
     cap = cv2.VideoCapture(filename)
@@ -35,18 +35,18 @@ def video_cap(filename):
     for i in range(frameCount):
         if (i % sampling_rate) == 0:
             sampled_frame[i // sampling_rate] = buf[i]
-            for j in range(3):
+           # for j in range(3):
                 # flip the image as it is capatured upside down
                 #sampled_frame[i // sampling_rate,:,:,j] = np.flipud(sampled_frame[i // sampling_rate,:,:,j])
                 #sampled_frame[i // sampling_rate,:,:,j] = np.fliplr(sampled_frame[i // sampling_rate,:,:,j])
-                sampled_frame[i // sampling_rate, :, :, j] = (sampled_frame[i // sampling_rate, :, :, j])
             sampled_frame[i // sampling_rate,:,:,:] = cv2.cvtColor(sampled_frame[i // sampling_rate,:,:,:], cv2.COLOR_BGR2RGB)
     return sampled_frame, frameCount//sampling_rate+1
 
+
 # define net for obj detection
 def detect_net():
-    net = model_zoo.get_model('yolo3_mobilenet1.0_voc', pretrained=True,ctx=gpu(0))
-    #net.load_parameters("yolo3_mobilenet1.0_voc_best.params")
+    net = model_zoo.get_model('yolo3_darknet53_voc', pretrained=True,ctx=gpu(0))
+    net.load_parameters("yolo3_darknet53_best.params")
     return net
 
 
@@ -56,36 +56,38 @@ def detect(net, frame_image):
     #ax = utils.viz.plot_bbox(img, bounding_boxs[0], scores[0],
                             #class_IDs[0], class_names=net.classes)
     #plt.show()
-
+    final_bounding_box = []
     i=0
-    final_bounding_box = bounding_boxs[0][0]
-    area_max = 0
+    scores_max =0
     for IDs in class_IDs[0]:
-        if IDs == 14 and scores[0][i] > 0.5:
-            [xmin, ymin, xmax, ymax]=bounding_boxs[0][i]
-            area = (xmax-xmin)*(ymax-ymin)*scores[0][i]
-            if area > area_max:
-                area_max = area
+        if IDs == 14 and scores[0][i] > 0.3:
+            if scores[0][i] > scores_max:
                 final_bounding_box = bounding_boxs[0][i]
+                scores_max = scores[0][i]
         i = i + 1
-
-    # obtain human boundbox
-    [xmin, ymin, xmax, ymax] = final_bounding_box
-    # clip images
-    clipped_image = img[int(ymin.asnumpy()):int(ymax.asnumpy()), int(xmin.asnumpy()):int(xmax.asnumpy()), :]
-    #plt.imshow(clipped_image)
-    #plt.show()
-    return clipped_image, [int(xmin.asnumpy()),int(ymin.asnumpy()), int(xmax.asnumpy()),int(ymax.asnumpy())], img
+    if len(final_bounding_box):
+        # obtain human boundbox
+        [xmin, ymin, xmax, ymax] = final_bounding_box
+        # clip images
+        clipped_image = img[int(ymin.asnumpy()):int(ymax.asnumpy()), int(xmin.asnumpy()):int(xmax.asnumpy()), :]
+        #plt.imshow(clipped_image)
+        #plt.show()
+        return clipped_image, [int(xmin.asnumpy()),int(ymin.asnumpy()), int(xmax.asnumpy()),int(ymax.asnumpy())], img
+    else:
+        return [],[],img
 
 
 def classify(net,image):
     # one gpu is necessary
-    transformed_img = data.transforms.presets.imagenet.transform_eval(mx.nd.array(image).as_in_context(gpu(0)))
-    pred = net(transformed_img)
-    ind = nd.argmax(pred, axis=1).astype('int')
-    action = class_list[ind.asscalar()]
-    print(action, nd.softmax(pred)[0][ind].asscalar())
-    return action,nd.softmax(pred)[0][ind].asscalar()
+    if  len(image):
+        transformed_img = data.transforms.presets.imagenet.transform_eval(mx.nd.array(image).as_in_context(gpu(0)))
+        pred = net(transformed_img)
+        ind = nd.argmax(pred, axis=1).astype('int')
+        action = class_list[ind.asscalar()]
+        print(action, nd.softmax(pred)[0][ind].asscalar())
+        return action,nd.softmax(pred)[0][ind].asscalar()
+    else:
+        return "None Action", 0.0
 
 
 def classification_net(classes):
@@ -97,8 +99,8 @@ def classification_net(classes):
     finetune_net.collect_params().reset_ctx(gpu(0))
     finetune_net.hybridize()
     # load net
-    finetune_net.load_parameters("fine_tune_net_2_best_epoch9.params")
-    # print(finetune_net)
+    finetune_net.load_parameters("fine_tune_net_v2_best.params")
+    print(finetune_net)
     return finetune_net
 
 # add action label
@@ -108,53 +110,57 @@ class_list.sort()
 
 
 # change to your video's name, mp4 format
-vid_file_name = "VID_20200315_175032"
+vid_file_name = "VID_20200401_202733"
 frames, clip_count = video_cap(vid_file_name + ".mp4")
 current_frame = frames[1]
 # detect and clip
 detect_net = detect_net()
 clip_image, bounding_box, img = detect(detect_net, current_frame)
-[xmin, ymin, xmax, ymax] = bounding_box
-# classify
-classification_net = classification_net(len(class_list))
-action_name = classify(classification_net,clip_image)
+if len(bounding_box):
+    [xmin, ymin, xmax, ymax] = bounding_box
+    # classify
+    classification_net = classification_net(len(class_list))
+    action_name = classify(classification_net,clip_image)
 
-# plot first frame's result
-plt.imshow(img)
-plt.gca().add_patch(patches.Rectangle((xmin,ymin),xmax-xmin,ymax-ymin,linewidth=1,edgecolor='r',facecolor='none'))
-plt.text(xmin+(xmax-xmin)/2, ymin - 20, action_name, fontsize=12,  color='red')
-plt.show()
+    # plot first frame's result
+    plt.imshow(img)
+    plt.gca().add_patch(patches.Rectangle((xmin,ymin),xmax-xmin,ymax-ymin,linewidth=1,edgecolor='r',facecolor='none'))
+    plt.text(xmin+(xmax-xmin)/2, ymin - 20, action_name, fontsize=12,  color='red')
+    plt.show()
 
 frames_vid = np.empty(shape=(clip_count,)+(512,910,3))
 i=0
 data_json = {}
-data_json['sleep'] = []
+data_json['action'] = []
 for current_frame in frames:
     # detect and clip
     clip_image, bounding_box, img = detect(detect_net, current_frame)
-    [xmin, ymin, xmax, ymax] = bounding_box
-    print(bounding_box)
-    if xmin < xmax and ymin < ymax and clip_image.shape[0]*clip_image.shape[1]>0 and xmin > 0 and ymin > 0:
-        # check if detection fails
-        # classify
-        print(clip_image.shape)
-        action_name,scores = classify(classification_net, clip_image)
-    # write bounding box
-        img = cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (255,0,0), 3)
-        cv2.putText(img, action_name, (int(xmin + (xmax - xmin) / 2), int(ymin - 20)), 1, 2, (255, 0, 0))
-        data_json['sleep'].append([float(i * 1 / 40), float(scores)])
+    if len(bounding_box):
+        [xmin, ymin, xmax, ymax] = bounding_box
+        print(bounding_box)
+        if xmin < xmax and ymin < ymax and clip_image.shape[0]*clip_image.shape[1]>0 and xmin > 0 and ymin > 0:
+            # check if detection fails
+            # classify
+            print(clip_image.shape)
+            action_name,scores = classify(classification_net, clip_image)
+
+        # write bounding box
+            img = cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (255,0,0), 3)
+            cv2.putText(img, action_name, (int(xmin + (xmax - xmin) / 2), int(ymin - 20)), 1, 2, (255, 0, 0))
+            data_json['action'].append([float(i * 1 / 40), action_name,float(scores)])
+        else:
+            data_json['action'].append([float(i * 1 / 40), action_name,float(0)])
     else:
-        data_json['sleep'].append([float(i * 1 / 40), float(0)])
+        data_json['action'].append([float(i * 1 / 40), action_name,float(0)])
     #uncomment this part to see each frame's prediction result
-    cv2.imshow('image', cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #cv2.imshow('image', cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
     # save frame to ndarray of vid
     frames_vid[i] = img
     i = i + 1
 
-
 with open(vid_file_name + "_detection"+'.json', 'w') as outfile:
     json.dump(data_json, outfile)
-#skvideo.io.vwrite(vid_file_name + "detection" + ".mp4", frames_vid,inputdict={},outputdict={'-r': str(30)})
+skvideo.io.vwrite(vid_file_name + "_v2_detection" + ".mp4", frames_vid,inputdict={},outputdict={'-r': str(30)})
 
